872d788843450281a300a8259517852064528a33
alexanderhmns <nenahomans@gmail.com>
2024-09-28 12:01:47 +0100

    Add files via upload

--- shapes.py
+++ shapes.py
@@ -1,1 +1,21 @@
-print ("alexander")
+import pgzrun
+
+
+
+WIDTH=500
+HEIGTH=500
+TITLE="SHAPES"
+
+
+r=100
+g=200
+b=241
+
+def draw():
+    rectangle = Rect(100,100,150,150)
+    screen.fill((r,g,b))
+    #screen.draw.filled_circle(rectangle,("red"))
+    screen.draw.filled_circle((300,300),70,"black")
+
+
+pgzrun.go()
