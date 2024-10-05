f9498d3a93608ea15a8df68bd0532d1c079371a8
alexanderhmns <nenahomans@gmail.com>
2024-10-05 11:56:20 +0100

    Update shapes.py

--- shapes.py
+++ shapes.py
@@ -2,8 +2,8 @@ import pgzrun
 
 
 
-WIDTH=500
-HEIGTH=500
+WIDTH=800
+HEIGHT=800
 TITLE="SHAPES"
 
 
@@ -14,8 +14,10 @@ b=241
 def draw():
     rectangle = Rect(100,100,150,150)
     screen.fill((r,g,b))
-    #screen.draw.filled_circle(rectangle,("red"))
-    screen.draw.filled_circle((300,300),70,"black")
-
+    screen.draw.filled_rect(rectangle,("green"))
+    screen.draw.circle((300,300),70,"black")
+    square = Rect (120,300,100,100)
+    screen.draw.filled_rect(square,("yellow"))
+    screen.draw.line((400,0,),(400,400), ("black"))
 
 pgzrun.go()
